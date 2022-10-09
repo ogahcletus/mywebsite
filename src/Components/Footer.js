06e165e419fa5de11c7ba423aed6e88149886ae3
Cletus Ogah <ogahcletus@gmail.com>
2022-10-09 15:23:44 +0100

    modify dependencies

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,9 +3,7 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import EmailIcon from  "@material-ui/icons/Email";
 import PhoneIcon from "@material-ui/icons/Phone";
-import "../styles/Footer.css";
-
-
+import "../styles/Footer.css"
 
 
 function Footer() {
